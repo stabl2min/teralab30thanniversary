78418493bb134f48b9ce42ada4d9c06c602423ac
stabl2min <128005243+stabl2min@users.noreply.github.com>
2026-05-03 12:17:32 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
+  base: '/teralab30thanniversary/',
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
